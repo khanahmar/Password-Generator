e975293769a5935ebdb23ea73c1ae02eb07a1993
Ahmar khan <ahmarbilal223344@gmail.com>
2022-12-27 16:51:50 +0500

    done

--- index.js
+++ index.js
@@ -1,5 +1,31 @@
+const randomFunc = {
+  lower: getRandomLower,
+  upper: getRandomUpper,
+  number: getRandomUpper,
+  symbol: getRandomSymbol,
+};
+
+// Getting random lower case
 function getRandomLower() {
   return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
 }
 
-console.log(getRandomLower());
+// Getting random upper case
+function getRandomUpper() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+// Getting random number case
+function getRandomNumber() {
+  return String.fromCharCode(Math.floor(Math.random() * 9) + 48);
+}
+
+function getRandomSymbol() {
+  const symbols = "!@#$^&*(){}[]=<>/,.";
+  return symbols[Math.floor(Math.random() * symbols.length)];
+}
+
+getRandomLower();
+getRandomUpper();
+getRandomNumber();
+getRandomSymbol();
+console.log(randomFunc);
