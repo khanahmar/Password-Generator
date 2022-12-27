8ac310072ad273218040c359c46ac6a055ec196e
Ahmar khan <ahmarbilal223344@gmail.com>
2022-12-27 17:10:40 +0500

    done

--- index.js
+++ index.js
@@ -1,3 +1,36 @@
+const resultEl = document.getElementById("result");
+const lengthEl = document.getElementById("length");
+const uppercaseEl = document.getElementById("uppercase");
+const lowercaseEl = document.getElementById("lowercase");
+const numbersEl = document.getElementById("numbers");
+const symbolsEl = document.getElementById("symbols");
+const generateBtn = document.getElementById("generate");
+const clipboardEl = document.getElementById("clipboard");
+
+generateBtn.addEventListener("click", (e) => {
+  const length = +lengthEl.value;
+  const hasLower = lowercaseEl.checked;
+  const hasUpper = uppercaseEl.checked;
+  const hasNumber = numbersEl.checked;
+  const hasSymbol = symbolsEl.checked;
+
+  resultEl.innerText = generatePassword(
+    hasLower,
+    hasUpper,
+    hasSymbol,
+    hasNumber,
+    length
+  );
+});
+
+// creating fucntion for generating pass
+function generatePassword(lower, upper, symbol, number, length) {
+  let generatedPassword = "";
+  const typesCount = lower + upper + symbol + number;
+  console.log(upper + true);
+  console.log(typesCount);
+}
+
 const randomFunc = {
   lower: getRandomLower,
   upper: getRandomUpper,
